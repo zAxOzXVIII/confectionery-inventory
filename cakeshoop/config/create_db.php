1e8ec7de5c24f749536eefdacb72e45c30c1eed7
zAxOzXVIII <119118178+zAxOzXVIII@users.noreply.github.com>
2023-01-27 22:07:08 -0400

    Add files via upload

---
+++ cakeshoop/config/create_db.php
@@ -0,0 +1,34 @@
+<?php 
+	$servidor='localhost';
+	$nmusuario='root';
+	$password="";
+	$db = "pasteleriaDB";
+	$conexion=new mysqli($servidor,$nmusuario,$password,$db);
+	if ($conexion->connect_error) {
+		die("Conexion fallida: " . $conexion->connect_error);
+	}
+	
+		$sql = "CREATE DATABASE IF NOT EXISTS pasteleriaDB";
+		if ($conexion->query($sql) === true) {
+		echo "Base de datos creada correctamente";
+		}else if($conexion->error){
+		die("Error al crear la base de datos " . $conexion->error);
+	}
+
+		$sql_table1 = "CREATE TABLE IF NOT EXISTS materials_cakeshop(
+		id INT(11) not null auto_increment PRIMARY KEY,
+		material VARCHAR(255) NOT NULL,
+		material_stock INT(11) NOT NULL,
+		date TIMESTAMP
+	)";
+	if ($conexion->query($sql_table1) === true) {
+		echo "Tabla creada correctamente";
+	}else{
+		die("Error al crear la tabla de la base de datos " . $conexion->error);
+	}
+
+
+
+
+
+?>
