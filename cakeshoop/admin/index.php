8035d7c812939f873e7992360b7d87355df2245e
zAxOzXVIII <119118178+zAxOzXVIII@users.noreply.github.com>
2023-01-29 14:29:32 -0400

    Add files via upload

---
+++ cakeshoop/admin/index.php
@@ -0,0 +1,16 @@
+<?php 
+include('./template/header.php');
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Admin</title>
+</head>
+<body>
+
+</body>
+</html>
+<?php 
+include('./template/footer.php');
+?>
